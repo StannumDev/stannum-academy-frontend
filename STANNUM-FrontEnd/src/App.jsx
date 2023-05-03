c909c24dbf806093ea65c179dd61c1723fa9b6ab
Mateo Lohezic <mateolohezic@gmail.com>
2023-05-03 18:50:22 -0300

    estructura

--- STANNUM-FrontEnd/src/App.jsx
+++ STANNUM-FrontEnd/src/App.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import { Route, Routes } from "react-router-dom";
 import './App.css'
+import NavBar from './Components/NavBar/NavBar';
 import Inicio from './Pages/Inicio/Inicio';
+import Test from './Pages/Test/test';
 
 function App() {
 
   return (
 
     <>
-
+    <NavBar />
     <Routes>
       <Route path="/" element={<Inicio/>} />
+      <Route path="/Test" element={<Test/>} />
     </Routes>
     
     </>
--- STANNUM-FrontEnd/src/Pages/Inicio/Inicio.jsx
+++ STANNUM-FrontEnd/src/Pages/Inicio/Inicio.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import './Inicio.css'
 
 function Inicio() {
 
   return (
-
     <>
-        Inicio
+      <h1 className='text-center mt-5'>RESOLVER ESTE TEST</h1>
+      <div className='text-center mt-5'>
+        <a href='/Test'><button className='btn btn-lg botonTest'>TEST</button></a>
+      </div>
     </>
-    
   )
 }
 
