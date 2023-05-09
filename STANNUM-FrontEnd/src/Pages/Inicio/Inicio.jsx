d6f2d38ce92b2cd221a3fba9ca154ef39cec3056
Mateo Lohezic <mateolohezic@gmail.com>
2023-05-09 15:39:29 -0300

    update inicio

--- STANNUM-FrontEnd/src/Pages/Inicio/Inicio.jsx
+++ STANNUM-FrontEnd/src/Pages/Inicio/Inicio.jsx
@@ -5,9 +5,10 @@ function Inicio() {
 
   return (
     <>
-      <h1 className='text-center mt-5'>RESOLVER ESTE TEST</h1>
-      <div className='text-center mt-5'>
-        <a href='/Test'><button className='btn btn-lg botonTest'>TEST</button></a>
+      <div className='d-flex flex-column align-items-center'>
+        <div className='text-center mt-5'><h1 className='tituloBienvenido'>¡Bienvenido a<br/><span className='tituloBienvenidoVerde'>Stannum Academy!</span></h1></div>
+        <div className='parrafoBienvenidaCentro text-center'><p>Este es el <b>centro de alto rendimiento</b> en donde podrás adquirir los conocimientos y las habilidades necesarias para llevar a tu emprendimiento al siguiente nivel</p></div>
+        <div className='text-center mt-5'><h2 className='tusPrimerosPasos'>Tus primeros pasos</h2></div>
       </div>
     </>
   )
