bff74ad8c49a2cd4d623aae1e27b8925c2285c93
Mateo Lohezic <mateolohezic@gmail.com>
2023-05-27 16:36:19 -0300

    deploy

--- STANNUM-FrontEnd/src/Pages/Inicio/Inicio.jsx
+++ STANNUM-FrontEnd/src/Pages/Inicio/Inicio.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import axios from "axios";
 import ModalEditarPrimeraVez from '../../Components/ModalEditarPrimeraVez/ModalEditarPrimeraVez'
 import './inicio.css'
 
 function Inicio() {
+
+  const [user, setUser] = useState({});
+
+  const token = localStorage.getItem('token');
+
+  useEffect(() => {
+      const getUser = async () =>{
+      if (token) {
+          const respuesta = await axios.post(`https://prueba-back-mateolohezic.up.railway.app/get-user/${token}`);
+          setUser(respuesta.data)
+      }}
+      getUser()
+  }, [token])
 
   return (
     <>
       <div className="bg"></div>
       <div className='d-flex flex-column align-items-center pb-5'>
         <div className='text-center tituloBienvenido'><h1>¡Bienvenido a</h1> <span>STANNUM Academy!</span></div>
-        {/* <ModalEditarPrimeraVez/> */}
+        {user.name === "Undefined" || user.surname === "Undefined" || user.biography === "Undefined" || user.territory === "Undefined" || user.venture === "Undefined" || user.birthdate === "Undefined" ? (
+          <ModalEditarPrimeraVez />
+        ) : null}
+
         <div className='parrafoBienvenidaCentro text-center'><p>Este es el <b>centro de alto rendimiento</b> en donde podrás adquirir los conocimientos y las habilidades necesarias para llevar a tu emprendimiento al siguiente nivel.</p></div>
         <div className='text-center mt-4 tusPrimerosPasos'><h2>Tus primeros pasos</h2></div>
         <div className='parrafoPrimerosPasos text-center'><p>Para poder comenzar tu camino dentro de Stannum Academy es necesario que completes los siguientes Tests de Dominio, ¡la información obtenida de ellos nos permitirá construir tu plantilla de jugador!.</p></div>
